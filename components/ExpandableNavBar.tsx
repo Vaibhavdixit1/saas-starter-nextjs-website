ee837988ac562485696784315c036b491eb9e38e
Vaibhavdixit1 <dixitvaibhav913@gmail.com>
2026-02-24 13:10:31 +0530

    Added basic navbar

---
+++ components/ExpandableNavBar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import React, { ReactNode, useMemo, useState } from "react";
+import { motion } from "framer-motion";
+import { FiMenu } from "react-icons/fi";
+import { Button } from "./shared/Button";
+import { DesktopLinks } from "./DesktopLinks";
+import { MobileLinks } from "./MobileLinks";
+import { Announcement } from "./Announcement";
+
+type LinkType = {
+  title: string;
+  sublinks: { title: string; href: string }[];
+};
+
+export const ExpandableNavBar = ({
+  children,
+  links,
+}: {
+  children?: ReactNode;
+  links: LinkType[];
+}) => {
+  const [hovered, setHovered] = useState<string | null>(null);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const activeSublinks = useMemo(() => {
+    if (!hovered) return [];
+    const link = links.find((l) => l.title === hovered);
+
+    return link ? link.sublinks : [];
+  }, [hovered, links]);
+
+  return (
+    <>
+      <div className="bg-indigo-600 pt-2">
+        <Announcement />
+        <nav
+          onMouseLeave={() => setHovered(null)}
+          className="rounded-t-2xl bg-white p-4"
+        >
+          <div className="flex items-start justify-between">
+            <div className="flex items-start">
+              <h1 className="text-xl font-bold text-indigo-600">MyApp</h1>
+              <DesktopLinks
+                links={links}
+                setHovered={setHovered}
+                hovered={hovered}
+                activeSublinks={activeSublinks}
+              />
+            </div>
+            <Button className="hidden md:block" intent="secondary" size="small">
+              <span className="font-bold">Get started - </span> no CC required
+            </Button>
+            <button
+              onClick={() => setMobileNavOpen((pv) => !pv)}
+              className="mt-0.5 block text-2xl text-black md:hidden"
+            >
+              <FiMenu />
+            </button>
+          </div>
+          <MobileLinks links={links} open={mobileNavOpen} />
+        </nav>
+      </div>
+      <motion.main layout>
+        <div className="bg-white">{children}</div>
+      </motion.main>
+    </>
+  );
+};
